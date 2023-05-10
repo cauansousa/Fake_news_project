57098857f23a8d13097eacc5aa023ae5621d3030
Cauan Sousa <103227067+cauansousa@users.noreply.github.com>
2023-05-09 22:25:58 -0300

    Final

--- src/IAGeradoraFakeNews.java
+++ src/IAGeradoraFakeNews.java
@@ -1,12 +1,3 @@
 public class IAGeradoraFakeNews {
     private int cor = 2;
-
-    public int[][] desenhaGeradora(int[][] mundo){
-        for(int i = 0; i < mundo.length; i++){
-            for(int j = 0; j < mundo[i].length; j++){
-                if((j >= 8 && j <= 19) && (i >= 3 && i <= 9)) mundo[i][j] = cor;
-            }
-        }
-        return mundo;
-    }
 }
--- src/Mundo.java
+++ src/Mundo.java
@@ -40,43 +40,57 @@ public class Mundo {
     private void setPessoas(ArrayList<Pessoa> pessoas) {this.pessoas = pessoas;}
 
     public void geraPessoas(){
+        // Define uma função que gera um número de pessoas e adiciona a lista de pessoas da classe
         int numeroPessoas = 100;
+        // Define o número de pessoas a serem geradas
         ArrayList<Pessoa> pessoas = new ArrayList<>();
+        // Inicializa uma lista vazia de objetos Pessoa
         Random rand = new Random();
+        // Inicializa um objeto da classe Random para gerar números aleatórios
         for(int i = 0; i < numeroPessoas; i++){
+            // Repete o processo de adicionar uma nova pessoa até chegar no número de pessoas definido
             pessoas.add(new PessoaBemInformada(rand.nextInt(57)+1,rand.nextInt(27)+1, 6, String.valueOf(i+100), 0, new ArrayList<>()));
+            // Adiciona uma nova pessoa aleatória à lista de pessoas
         }
         setPessoas(pessoas);
+        // Define a lista de pessoas da classe como a lista gerada
     }
 
+
     public int[][] desenhaPessoas(ArrayList<Pessoa> pessoas) {
+        // Define uma função que recebe uma lista de objetos Pessoa e retorna uma matriz de inteiros representando um mapa onde cada posição contém a cor da pessoa que está naquela posição
         int[][] mapa = new int[this.mundo.length][60];
+        // A matriz do mapa é inicializada como uma cópia do atributo 'mundo' da classe que contém a função
         for (int i = 0; i < mapa.length; i++) {
             System.arraycopy(mundo[i], 0, mapa[i], 0, mundo[i].length);
         }
+        // A função percorre a lista de pessoas recebida e define a cor da pessoa na posição correspondente na matriz 'mapa'
         pessoas.forEach(p -> mapa[p.getY()][p.getX()] = p.getCor());
+        // Por fim, a função retorna a matriz 'mapa'
         return mapa;
     }
 
+
+    // Esta função verifica a localização de uma pessoa e atualiza seu status de imunidade ou substitui a pessoa por uma nova com novos parâmetros.
     public Pessoa checaPessoas(Pessoa p){
+        // verifica se a pessoa está dentro das coordenadas específicas
         if((p.getX() >= 42 && p.getX() <= 52) && (p.getY() >= 12 && p.getY() <= 18)){
-            if(p instanceof PessoaBemInformada){
-                return new PessoaMalInformada(p.getX(), p.getY(), 7, p.getWhatsappID(), p.getTempoImune(), p.getAgendaContatos());
-            }
+            // se a pessoa não for uma instância de PessoaBemInformada, define seu tempo de imunidade como 31 e retorna null
+            p.setCor(7);
             p.setTempoImune(31);
             return null;
-        }else if (p.getTempoImune() == 0) {
+        }else if (p.getTempoImune() == 0) { // se a pessoa não estiver nas coordenadas especificadas e seu tempo de imunidade for zero
             if(p.getX() >=7 && p.getX() <= 20){
-                if(p.getY() >= 2 && p.getY() <= 10){
+                if(p.getY() >= 2 && p.getY() <= 10){ // se a pessoa estiver dentro dessas coordenadas, cria e retorna uma nova PessoaMalInformada com coordenadas, tempo de imunidade e outros atributos da pessoa original
                     if (p instanceof PessoaBemInformada) {
                         Pessoa p1 = new PessoaMalInformada(p.getX(), p.getY(), 5, p.getWhatsappID(), p.getTempoImune(), p.getAgendaContatos());
-                        checaAgenda(p1);
+                        checaAgenda(p1); // verifica e atualiza a agenda de contatos da nova pessoa criada
                         return p1;
                     }
-                } else if (p.getY() >= 18 && p.getY() <= 26) {
+                } else if (p.getY() >= 18 && p.getY() <= 26) { // se a pessoa estiver dentro dessas coordenadas, cria e retorna uma nova PessoaBemInformada com coordenadas, tempo de imunidade e outros atributos da pessoa original
                     if (p instanceof PessoaMalInformada) {
                         Pessoa p1 = new PessoaBemInformada(p.getX(), p.getY(), 6, p.getWhatsappID(), p.getTempoImune(), p.getAgendaContatos());
-                        checaAgenda(p1);
+                        checaAgenda(p1); // verifica e atualiza a agenda de contatos da nova pessoa criada
                         return p1;
                     }
                 }
@@ -100,17 +114,24 @@ public class Mundo {
         return null;
     }
 
+    // Essa função adiciona contatos à agenda de uma pessoa, com base na proximidade geográfica dos contatos existentes.
     public void adicionaContatos(Pessoa pessoa){
+        // itera sobre a lista de pessoas (atributo da classe)
         for(Pessoa p : this.pessoas){
+            // verifica se a coordenada X da pessoa a ser adicionada é igual ou tem uma diferença de uma unidade da coordenada X da pessoa atual da iteração
             if(pessoa.getX() == p.getX() || pessoa.getX()+1 == p.getX() || pessoa.getX()-1 == p.getX()){
+                // verifica se a coordenada Y da pessoa a ser adicionada é igual ou tem uma diferença de uma unidade da coordenada Y da pessoa atual da iteração
                 if(pessoa.getY() == p.getY() || pessoa.getY()+1 == p.getY() || pessoa.getY()-1 == p.getY()){
+                    // verifica se a pessoa atual não é a mesma que a pessoa a ser adicionada
                     if(!Objects.equals(pessoa.getWhatsappID(), p.getWhatsappID())){
+                        // verifica se o whatsappID da pessoa atual já não está na lista de contatos da pessoa a ser adicionada, e adiciona se não estiver
                         if(!pessoa.getAgendaContatos().contains(p.getWhatsappID())) pessoa.addContato(p.getWhatsappID());
                     }
                 }
             }
         }
     }
+
 
     public void checaAgenda(Pessoa p){
         for(Pessoa p2 : this.pessoas){
@@ -168,10 +189,13 @@ public class Mundo {
                 if(p instanceof PessoaBemInformada) ((PessoaBemInformada) p).move();
                 else ((PessoaMalInformada) p).move();
             }
-            if(p.getTempoImune() != 0) {
-                p.setTempoImune(p.getTempoImune()-1);
+            if(p.getTempoImune() != 0) p.setTempoImune(p.getTempoImune()-1);
+
+            if(p.getTempoImune() == 0 && p.getCor() == 7){
+                if (p instanceof PessoaBemInformada) p.setCor(6);
+                else p.setCor(5);
+
             }
-            if(p.getTempoImune() == 0 && p.getCor() == 1) p.setCor(6);
         }
     }
 }
